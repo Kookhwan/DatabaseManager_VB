9fb2236465eb0569db018e3f36577a4dd33beee6
Evan Im <tatata1358@gmail.com>
2023-05-16 00:12:31 -0400

    Initialization

---
+++ DatabaseManager/frmMain.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvDatabase = New System.Windows.Forms.DataGridView()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvDatabase, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvDatabase
+        '
+        Me.dgvDatabase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDatabase.Location = New System.Drawing.Point(3, 210)
+        Me.dgvDatabase.Name = "dgvDatabase"
+        Me.dgvDatabase.Size = New System.Drawing.Size(795, 207)
+        Me.dgvDatabase.TabIndex = 4
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(3, 423)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(795, 25)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(488, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(310, 192)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.dgvDatabase)
+        Me.Name = "frmMain"
+        Me.Text = "frmMain"
+        CType(Me.dgvDatabase, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dgvDatabase As DataGridView
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
