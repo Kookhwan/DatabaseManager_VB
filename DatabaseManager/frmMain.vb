9b68d526c7ac638a3426883652fef15add6b2a3a
Evan Im <tatata1358@gmail.com>
2023-05-16 01:08:55 -0400

    create functions for loading db lists

--- DatabaseManager/frmMain.vb
+++ DatabaseManager/frmMain.vb
@@ -7,6 +7,36 @@ Imports Microsoft.Office.Interop
 
 Public Class frmMain
 
+    Private Sub psubLoadDatabase()
+
+        Dim conLocalSQL As New SqlConnection("Data Source=DBSERVER\AWTESTZONE17;Persist Security Info=True;User ID=sa;Password=&&AW1975&&")
+        Dim sqlReader As SqlDataReader = Nothing
+        Dim strQuery As String
+
+        Try
+
+            conLocalSQL.Open()
+
+            strQuery = ""
+            strQuery = strQuery & "SELECT name "
+            strQuery = strQuery & "FROM   sys.databases "
+            strQuery = strQuery & "WHERE  name LIKE 'ArtwoodsSQL%'"
+            strQuery = strQuery & "ORDER BY create_date ASC "
+
+            dgvDatabase.DataSource = gfunDataTableSQL(strQuery, conLocalSQL)
+
+            conLocalSQL.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+
+
+
+
+    End Sub
+
 
 
 End Class
--- DatabaseManager/modDatabase.vb
+++ DatabaseManager/modDatabase.vb
@@ -5,4 +5,21 @@ Imports System.Data.SqlClient
 
 Module modDatabase
 
+    Public Function gfunDataTableSQL(ByVal strSource As String, ByVal oDatabase As SqlConnection) As DataTable
+
+        Dim command As New SqlCommand(strSource, oDatabase)
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable()
+
+        Try
+            adapter.Fill(table)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        gfunDataTableSQL = table
+
+    End Function
+
+
 End Module
